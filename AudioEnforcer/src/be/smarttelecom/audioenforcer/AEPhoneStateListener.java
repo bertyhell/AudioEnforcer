670a00396df4b7e0d37948ff65c4dbfa20b2f6bb
Bert Verhelst <verhelstbert@gmail.com>
2014-08-25 16:56:20 +0200

    catch exception and ignore

--- AudioEnforcer/src/be/smarttelecom/audioenforcer/AEPhoneStateListener.java
+++ AudioEnforcer/src/be/smarttelecom/audioenforcer/AEPhoneStateListener.java
@@ -1,7 +1,12 @@
 package be.smarttelecom.audioenforcer;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import android.content.Context;
 import android.media.AudioManager;
+import android.os.Environment;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -15,29 +20,43 @@ public class AEPhoneStateListener extends PhoneStateListener {
 	public AEPhoneStateListener(Context context) {
 		_context = context;
 		_audioManager = (AudioManager) _context.getSystemService(Context.AUDIO_SERVICE);
+
+		Thread.currentThread().setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+			@Override
+			public void uncaughtException(Thread thread, Throwable ex) {
+
+				PrintWriter pw;
+				try {
+					pw = new PrintWriter(new FileWriter(Environment.getExternalStorageDirectory() + "/audioenforcer.log", true));
+					ex.printStackTrace(pw);
+					pw.flush();
+					pw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-	
 
 	public void onCallStateChanged(int state, String incomingNumber) {
 
 		switch (state) {
 		case TelephonyManager.CALL_STATE_IDLE:
 			Log.d("DEBUG", "IDLE");
-			
+
 			enforceAudioSettings();
-			
-//			int maxvolume = _audioManager.getStreamMaxVolume(AudioManager.STREAM_RING);
-//			int currentVolume = _audioManager.getStreamVolume(AudioManager.STREAM_RING);
-//			Log.d("DEBUG", "volume: " + currentVolume + " " + maxvolume);
-//			
-//			_audioManager.setStreamVolume(AudioManager.STREAM_RING, maxvolume, 0);
+
+			//			int maxvolume = _audioManager.getStreamMaxVolume(AudioManager.STREAM_RING);
+			//			int currentVolume = _audioManager.getStreamVolume(AudioManager.STREAM_RING);
+			//			Log.d("DEBUG", "volume: " + currentVolume + " " + maxvolume);
+			//			
+			//			_audioManager.setStreamVolume(AudioManager.STREAM_RING, maxvolume, 0);
 
 			break;
 		}
 	}
 
-
-	private void enforceAudioSettings() {		
+	private void enforceAudioSettings() {
 		switch (AudioEnforcerSettings.getAudioSetting()) {
 		case Low:
 			AudioEnforcerSettings.setAudioSetting(AudioProfiles.Low);
@@ -54,6 +73,5 @@ public class AEPhoneStateListener extends PhoneStateListener {
 			_audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
 			_audioManager.setStreamVolume(AudioManager.STREAM_RING, _audioManager.getStreamMaxVolume(AudioManager.STREAM_RING), 0);
 		}
-		
 	}
 }
